6aabe296a3ef29ac8234329d839424d669135a9b
Ryn-Bghl <rayan.boughalia@gmail.com>
2024-10-24 19:58:17 +0200

    "Updated index.html to replace anchor tag with button for 'Clear All' functionality and added reset function in main.js to clear input fields"

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ var repaymentInput = document.querySelector(".form__main__input__repayment");
 var interestOnlyInput = document.querySelector(
   ".form__main__input__interest-only"
 );
+var mortgageAmountInput = document.querySelector("#mortgage_amount");
+var mortgageTermInput = document.querySelector("#mortgage_term");
+var interestRateInput = document.querySelector("#interest_rate");
+var mortgageTypeRadioInputs = document.querySelectorAll(".mortgage_type");
 
 function changeColor() {
   if (
@@ -18,4 +22,12 @@ function changeColor() {
     repaymentInput.style.border = "1px solid #d7da2f";
     interestOnlyInput.style.border = "";
   }
+}
+
+function reset() {
+  mortgageAmountInput.value = "";   
+  mortgageTermInput.value = "";
+  interestRateInput.value = "";
+  mortgageTypeRadioInputs[0].childNodes[3].childNodes[1].checked = false;
+  mortgageTypeRadioInputs[0].childNodes[5].childNodes[1].checked = false;
 }
